5c66889fb04a7ba8861c0305a2901b6d6837bd4d
ranashehrozfazil <37393086+ranashehrozfazil@users.noreply.github.com>
2018-05-07 18:41:16 +1000

    Add files via upload

---
+++ BattleShipsCS_2018/src/TestingShipDestroyed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+    [TestFixture]
+    class TestingShipDestroyed
+    {
+        [TestCase()]
+        public void IsDestroyedTest()
+        {
+          Ship testShip = new Ship(ShipName.AircraftCarrier);
+           for (int i = 0; i < 5; i++) { testShip.Hit(); }
+           bool expected = true;
+           bool actual = testShip.IsDestroyed;
+          Assert.AreEqual(expected, actual);
+          }
+    }
+
